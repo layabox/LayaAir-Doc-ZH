37074b36d1018708e1f5c1db1986762ad6cbbe80
LayaCharley <399050@qq.com>
2026-07-16 12:21:07 +0800

    优化文档：LayaAir3引擎功能概述

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/showcase/unity-export.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fshowcase%2Funity-export.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/showcase/live-demo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fshowcase%2Flive-demo.mdx&astroContentModuleFlag=true")],
+["src/content/docs/showcase/unity-export.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fshowcase%2Funity-export.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/unity-plugin/animation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Funity-plugin%2Fanimation.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/unity-plugin/custom-shader.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Funity-plugin%2Fcustom-shader.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/unity-plugin/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Funity-plugin%2Findex.mdx&astroContentModuleFlag=true")],
@@ -10,9 +10,9 @@ export default new Map([
 ["src/content/docs/unity-plugin/known-issues.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Funity-plugin%2Fknown-issues.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/unity-plugin/material-shader.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Funity-plugin%2Fmaterial-shader.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/unity-plugin/mesh.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Funity-plugin%2Fmesh.mdx&astroContentModuleFlag=true")],
-["src/content/docs/unity-plugin/overview.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Funity-plugin%2Foverview.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/unity-plugin/particle.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Funity-plugin%2Fparticle.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/unity-plugin/sprite-2d.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Funity-plugin%2Fsprite-2d.mdx&astroContentModuleFlag=true")],
+["src/content/docs/unity-plugin/overview.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Funity-plugin%2Foverview.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/roadmap/beginner/checklist.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Froadmap%2Fbeginner%2Fchecklist.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/roadmap/beginner/core-concepts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Froadmap%2Fbeginner%2Fcore-concepts.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/guides/roadmap/beginner/first-project.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fguides%2Froadmap%2Fbeginner%2Ffirst-project.mdx&astroContentModuleFlag=true")],
